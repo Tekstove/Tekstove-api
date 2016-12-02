e9bdcb42bd97ba2e09e652e8cbdcfebfeeb3ee0a
Angel Koilov (po_taka) <angel.koilov@gmail.com>
2016-12-02 21:34:10 +0200

    To symfony 3.2 (#6)
    * migrate to symfony 3.2

--- web/app.php
+++ web/app.php
@@ -2,27 +2,13 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
-/**
- * @var Composer\Autoload\ClassLoader
- */
+/** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
-// Enable APC for autoloading to improve performance.
-// You should change the ApcClassLoader first argument to a unique prefix
-// in order to prevent cache key conflicts with other applications
-// also using APC.
-/*
-$apcLoader = new Symfony\Component\ClassLoader\ApcClassLoader(sha1(__FILE__), $loader);
-$loader->unregister();
-$apcLoader->register(true);
-*/
-
-//require_once __DIR__.'/../app/AppCache.php';
-
 // I know this is ugly...but...sry...
-function errorToException($errNumber, $errMsg, $errFile, $errLine) {
-    
+function errorToException($errNumber, $errMsg, $errFile, $errLine)
+{
     $errorMsg = "Error#{$errNumber}. $errMsg in $errFile : $errLine";
     \error_log($errorMsg);
     

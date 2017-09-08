827c920077a0b6f8137faa79c9bdfb67db55d6d4
po_taka <angel.koilov@gmail.com>
2017-09-08 23:16:40 +0300

    PM history

--- Test/Phpunit/Test/ApiBundle/Repo/CriteriaGeneratorTest.php
+++ Test/Phpunit/Test/ApiBundle/Repo/CriteriaGeneratorTest.php
@@ -6,26 +6,43 @@ use Tekstove\ApiBundle\Repo\CriteriaGenerator;
 use Mockery;
 use Propel\Runtime\Map\TableMap;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Propel\Runtime\ActiveQuery\ModelCriteria;
 
 class CriteriaGeneratorTest extends MockeryTestCase
 {
     public function testGenerateCompositeCriterion()
     {
-        // this test is not very useful
-
         $generatorMockBuilder = $this->getMockBuilder(CriteriaGenerator::class);
         $generatorMockBuilder->disableOriginalConstructor();
         $generatorMockBuilder->setMethods(['getSqlFieldNameFromPhpName']);
         $generator = $generatorMockBuilder->getMock();
         $generator->expects($this->once())
                   ->method('getSqlFieldNameFromPhpName')
-                  ->willReturn('mockedValue');
+                  ->willReturn('mockedSqlField');
 
-        $modelCriteriaMockBuilder = $this->getMockBuilder(\Propel\Runtime\ActiveQuery\ModelCriteria::class);
+        $modelCriteriaMockBuilder = $this->getMockBuilder(ModelCriteria::class);
         $modelCriteriaMockBuilder->disableOriginalConstructor();
-        $modelCriteriaMockBuilder->setMethods(['getTableMap', 'addCond', 'combine']);
+        $modelCriteriaMockBuilder->setMethods(
+            [
+                'getTableMap',
+                'addCond',
+                'combine',
+                'getNewCriterion',
+            ]
+        );
+
+        $criterionMock = Mockery::mock(\Propel\Runtime\ActiveQuery\Criterion\AbstractCriterion::class);
 
         $modelCriteriaMock = $modelCriteriaMockBuilder->getMock();
+
+        $modelCriteriaMock->expects($this->once())
+                ->method('getNewCriterion')
+                ->with(
+                    $this->equalTo('mockedSqlField'),
+                    $this->equalTo('2'),
+                    $this->equalTo('=')
+                )
+                ->willReturn($criterionMock);
 
         $tableMapMockBuilder = $this->getMockBuilder(TableMap::class);
         $tableMapMockBuilder->disableOriginalConstructor();
@@ -36,7 +53,11 @@ class CriteriaGeneratorTest extends MockeryTestCase
                           ->wilLReturn($tableMock);
 
         $modelCriteriaMock->expects($this->once())
-                          ->method('addCond');
+                          ->method('addCond')
+                          ->with(
+                                $this->anything(),
+                                $this->equalTo($criterionMock)
+                            );
 
         $modelCriteriaMock->expects($this->once())
                           ->method('combine');
@@ -53,6 +74,51 @@ class CriteriaGeneratorTest extends MockeryTestCase
                 ],
             ],
             $modelCriteriaMock
+        );
+    }
+
+    public function testGenerateCompositeCriterionNotNull()
+    {
+        $tableMap = Mockery::mock(TableMap::class);
+
+        $generatorMockBuilder = $this->getMockBuilder(CriteriaGenerator::class);
+        $generatorMockBuilder->disableOriginalConstructor();
+        $generatorMockBuilder->setMethods(['getSqlFieldNameFromPhpName']);
+        $generator = $generatorMockBuilder->getMock();
+        $generator->expects($this->once())
+                  ->method('getSqlFieldNameFromPhpName')
+                  ->willReturn('title');
+
+        $modelCriteria = Mockery::mock(ModelCriteria::class);
+        $modelCriteria->shouldReceive('getTableMap')
+                      ->andReturn($tableMap);
+
+        $modelCriteria->shouldReceive('getNewCriterion')
+                      ->once()
+                      ->with(
+                            'title',
+                            null,
+                            \Propel\Runtime\ActiveQuery\Criteria::ISNOTNULL
+                        );
+
+        $modelCriteria->shouldReceive('addCond')
+                      ->once();
+
+        $modelCriteria->shouldReceive('combine')
+                      ->once();
+
+        $generator->generateCompositeCriterion(
+            [
+                'operator' => 'or',
+                'value' => [
+                    [
+                        'operator' => 'NOT_NULL',
+                        'value' => '0',
+                        'field' => 'title',
+                    ],
+                ]
+            ],
+            $modelCriteria
         );
     }
 

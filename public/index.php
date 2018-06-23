659c46f2a15cdef8fd6243f4a3789e726263b8d7
po_taka <angel.koilov@gmail.com>
2018-06-23 20:27:05 +0300

    error handler fixes

--- public/index.php
+++ public/index.php
@@ -22,8 +22,44 @@ if ($debug) {
     umask(0000);
 
     Debug::enable();
+} else {
+    // I know this is ugly...but...sry...
+    function errorToException($errNumber, $errMsg, $errFile, $errLine)
+    {
+        $errorMsg = "Error#{$errNumber}. $errMsg in $errFile : $errLine";
+        \error_log($errorMsg);
+
+        if (error_reporting() === 0) {
+            return true;
+        }
+         throw new \Exception($errMsg);
+    }
+
+    set_error_handler('errorToException');
 }
 
+$_SERVER['TRUSTED_PROXIES'] = implode(
+    ',',
+    [
+        "79.98.109.18",
+        "127.0.0.1",
+        "103.21.244.0/22",
+        "103.22.200.0/22",
+        "103.31.4.0/22",
+        "104.16.0.0/12",
+        "108.162.192.0/18",
+        "131.0.72.0/22",
+        "141.101.64.0/18",
+        "162.158.0.0/15",
+        "172.64.0.0/13",
+        "173.245.48.0/20",
+        "188.114.96.0/20",
+        "190.93.240.0/20",
+        "197.234.240.0/22",
+        "198.41.128.0/17",
+        "199.27.128.0/21",
+    ]
+);
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
 }

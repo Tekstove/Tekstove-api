32a6d00d70b8c84ec1fc07606f34b4d7224e88f7
po_taka <angel.koilov@gmail.com>
2018-12-19 23:04:26 +0200

    wip

--- src/Controller/V4/Lyric/LyricController.php
+++ src/Controller/V4/Lyric/LyricController.php
@@ -4,12 +4,12 @@ namespace App\Controller\V4\Lyric;
 
 use App\Entity\Lyric\Lyric;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class LyricController extends AbstractController
 {
-    public function view(Request $request, string $id)
+    public function view(SerializerInterface $serializer, string $id)
     {
         $repo = $this->getDoctrine()->getRepository(Lyric::class);
         $entity = $repo->findOneBy(['id' => $id]);
@@ -20,6 +20,25 @@ class LyricController extends AbstractController
                 'sendDate' => $entity->getSendDate()->getTimestamp(),
             ],
         ];
+
+
+        $r = $serializer->serialize(
+            $entity,
+            'json',
+            [
+                'groups' => [
+                    'g2',
+                    'g1',
+                ],
+            ]
+        );
+
+
+
+        dump($serializer);
+        dump($entity);
+        dump($r); die;
+
 
         $response = new Response(json_encode($returnData));
 

7799c7ef25dd5c41a320652d705ba8226fc98b5b
po_taka <angel.koilov@gmail.com>
2019-01-21 22:02:03 +0200

    artist list v4

--- src/Controller/V4/Artist/ArtistController.php
+++ src/Controller/V4/Artist/ArtistController.php
@@ -4,10 +4,18 @@ namespace App\Controller\V4\Artist;
 
 use App\Controller\V4\TekstoveController;
 use App\Entity\Artist\Artist;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArtistController extends TekstoveController
 {
+    public function indexAction(Request $request): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Artist::class);
+
+        return $this->handleRepository($repo);
+    }
+
     public function getAction(string $id): Response
     {
         $repo = $this->getDoctrine()->getRepository(Artist::class);

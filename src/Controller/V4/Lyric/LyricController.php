65ce5b65ad9d5f11a73855af5afb617b3d333a63
Angel Koilov (po_taka) <angel.koilov@gmail.com>
2019-08-08 00:30:27 +0300

    auth_filter (#103)

--- src/Controller/V4/Lyric/LyricController.php
+++ src/Controller/V4/Lyric/LyricController.php
@@ -3,9 +3,12 @@
 namespace App\Controller\V4\Lyric;
 
 use App\Controller\V4\TekstoveController;
+use App\Entity\AuthorizationInterface;
 use App\Entity\Lyric\Lyric;
 use App\Entity\Lyric\Redirect;
 use App\EventDispatcher\Lyric\LyricEvent;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,18 +17,55 @@ class LyricController extends TekstoveController
 {
     public function indexAction(Request $request): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Lyric::class);
+        $qb = $repo->createQueryBuilder('d');
         $filters = $request->query->get('filters', []);
-        foreach ($filters as &$filter) {
+        $filterKeysToDelete = [];
+        foreach ($filters as $filterKey => &$filter) {
             if ($filter['field'] == 'ArtistId' || $filter['field'] == 'artist') {
                 $filter['field'] = 'd.artistLyrics.artist';
             } elseif ($filter['field'] == 'publisher') {
                 $filter['field'] = 'd.publishers.id';
+            } elseif ($filter['field'] == 'onlyAuthorized') {
+                /* @var $qb QueryBuilder */
+                $qb->leftJoin(
+                    'd.artistLyrics',
+                    'permissions_al'
+                );
+
+                $qb->leftJoin(
+                    'permissions_al.artist',
+                    'permissions_artist',
+                    Query\Expr\Join::WITH,
+                    'permissions_artist.authorization = :authorizationStatus'
+                );
+
+                $qb->setParameter('authorizationStatus', AuthorizationInterface::AUTHORIZATION_ALLOWED);
+
+                $qb->leftJoin(
+                    'd.publishers',
+                    'permissions_publisher',
+                    Query\Expr\Join::WITH,
+                    'permissions_publisher.authorization = :authorizationStatus'
+                );
+
+                $qb->andWhere(
+                    $qb->expr()->orX(
+                        $qb->expr()->isNotNull('permissions_publisher.id'),
+                        $qb->expr()->isNotNull('permissions_artist.id')
+                    )
+                );
+
+                $filterKeysToDelete[] = $filterKey;
             }
+        }
+
+        foreach ($filterKeysToDelete as $key) {
+            unset($filters[$key]);
         }
         $request->query->set('filters', $filters);
 
-        $repo = $this->getDoctrine()->getRepository(Lyric::class);
-        return $this->handleRepository($repo);
+        return $this->handleQueryBuilder($qb);
     }
 
     public function getAction(EventDispatcherInterface $eventDispatcher, string $id): Response

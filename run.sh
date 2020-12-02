c429232bfa77afa90160a7c91cfb746ac620b07f
po_taka <angel.koilov@gmail.com>
2020-12-03 00:11:17 +0200

    migrating to php 74

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 
 set -e
 
+# If the script is run with sudo, UID is 0. This is an issue when running
+# "usermod -u $WEB_UID www-data" in the web container.
+# In this case assign WEB_UID to 1000
+[[ $UID == 0 ]] && export WEB_UID=1000 || export WEB_UID=$UID
+
 cd docker
-docker-compose pull
 docker-compose build
 docker-compose up

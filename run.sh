946400716f0f382c6f81f4eedcdacdd4fb5aed8b
po_taka <angel.koilov@gmail.com>
2020-12-04 19:08:27 +0200

    changed project name to tekstove

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PROJECT_NAME=tekstove-api
+PROJECT_NAME=tekstove
 
 # If the script is run with sudo, UID is 0. This is an issue when running
 # "usermod -u $WEB_UID www-data" in the web container.
@@ -15,7 +15,7 @@ docker build --tag=tekstove-api-php ./php/
 docker-compose -p $PROJECT_NAME build
 
 if [[ "$1" == "-p" ]]; then
-    docker-compose -p PROJECT_NAME -f docker-compose.yml -f up
+    docker-compose -p $PROJECT_NAME -f docker-compose.yml -f up
 else
-    docker-compose -p PROJECT_NAME -f docker-compose.yml -f docker-compose-dev.yml up
+    docker-compose -p $PROJECT_NAME -f docker-compose.yml -f docker-compose-dev.yml up
 fi

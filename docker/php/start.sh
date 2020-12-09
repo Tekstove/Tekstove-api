6fc2aeba76363acd1fea35d29aeee1c63e0a9a29
po_taka <angel.koilov@gmail.com>
2020-12-09 19:38:57 +0200

    config changes

--- docker/php/start.sh
+++ docker/php/start.sh
@@ -4,6 +4,6 @@ set -e
 
 usermod -u $WEB_UID www-data
 
-chown www-data -R /var/www/tekstove-api
+chown www-data -R /var/www
 su -c "composer install -d /var/www/tekstove-api" -s /bin/sh www-data
 apachectl -D FOREGROUND

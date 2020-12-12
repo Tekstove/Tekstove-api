85b73fb7674650844732e741255f148a277d6b0b
Angel Koilov (po_taka) <angel.koilov@gmail.com>
2020-12-12 16:40:17 +0200

    migrating to php 74 (#107)
    * migrating to php 74
    
    * migrate tests
    
    * separated dev & prod env
    
    * added lock files
    
    * changed project name to tekstove
    
    * removed xdebug from default build
    
    * config changes
    
    * prod flag bugfix
    
    * removed unsued env variables
    
    * deleted unused prod conf
    
    * use php instead of php-cgi

--- docker/php/start.sh
+++ docker/php/start.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-chown site_tekstove_api -R /var/www/tekstove-api
-su -c "composer install -d /var/www/tekstove-api" -s /bin/sh site_tekstove_api
-/etc/init.d/apache2 restart
-tail -F /var/log/apache2/error.log -F /var/www/tekstove-api/var/log/dev.log
+usermod -u $WEB_UID www-data
+
+chown www-data -R /var/www
+su -c "composer install -d /var/www/tekstove-api" -s /bin/sh www-data
+apachectl -D FOREGROUND
